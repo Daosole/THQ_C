9c0ce95dd5b358eef535c7f283ed4db607de6d7e
V-T0 <keivong@outlook.com>
2021-10-23 23:21:27 +0800

    格式化代码

--- Chapter4/4.4.cpp
+++ Chapter4/4.4.cpp
@@ -1,24 +1,24 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main(void)
 {
 	int max(int x, int y);
-	int a,b,c,m;
+	int a, b, c, m;
 	printf("请输入三个整数\n");
-	scanf("%d,%d,%d",&a,&b,&c);
-	m = max(a,b); //m 为 a b 中的较大值
-	m = max(m,c); //m 为 m 与 c 中的较大值
-	printf("max=%d\n",m);
-	
+	scanf("%d,%d,%d", &a, &b, &c);
+	m = max(a, b); //m 为 a b 中的较大值
+	m = max(m, c); //m 为 m 与 c 中的较大值
+	printf("max=%d\n", m);
+
 	return 0;
-	
 }
 
 int max(int x, int y)
 {
 	int z;
-	if ( x > y)
+	if (x > y)
 		z = x;
-	else z = y;
-	
+	else
+		z = y;
+
 	return z;
 }
