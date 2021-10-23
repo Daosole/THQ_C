a9f5c540c3f36ece9db12fa21bf8376e839691cd
V-T0 <keivong@outlook.com>
2021-10-23 12:57:54 +0800

    add new file

---
+++ Chapter5/5.6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+    double factorial(double x); //求阶乘的函数
+    double sum,i,j;
+    sum = 0;
+    for(i = 1; i <= 20; i++)
+    {
+        j = factorial(i);
+        sum = sum + j; //求和
+    }
+    printf("%lf\n",sum);
+
+    return 0;
+}
+
+double factorial(double x)
+{
+    double a,s;
+    for( a = 1, s = 1; a <= x; a++)
+    {
+        s = s*a;
+    }
+
+    return s;
+}
---
+++ Chapter5/5.7.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+    double sum,s1,s2,s3,i,j,k;
+    s1 = s2 = s3 =0;
+    for(i = 1; i <= 100; i++)
+    {
+        s1 = s1 + i;
+    }
+
+    for(j = 1; j <= 50; j++)
+    {
+        s2 = s2 + j*j;
+    }
+
+    for(k = 1; k <= 10; k++)
+    {
+        s3 = s3 + 1/k;
+    }
+
+    sum = s1 + s2 + s3;
+
+    printf("%lf\n",sum);
+
+    return 0;
+}
