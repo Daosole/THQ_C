3639f908776502dc995998b08a580686f0f6c098
V-T0 <keivong@outlook.com>
2021-10-23 10:44:57 +0800

    add new file

---
+++ Chapter5/5.13.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	float x,x0,a;
+//	double n;
+	printf("Enter the a value:");
+	scanf("%f",&a);
+	x0 = a/2; //赋初值
+	x = (x0 + a/x0)/2;  // 循环之前先进行一次
+	do
+	{
+		x0 = x;
+		x = (x0 + a/x0)/2;
+	} while(fabs(x-x0) > 1e-5);
+	
+//	printf("%f",fabs(i-j));
+	printf("%f",x);
+	
+	return 0;
+	
+}
