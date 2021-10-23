cfbf48d097238dfbced674cbb07bc5a880598052
V-T0 <keivong@outlook.com>
2021-10-23 18:54:49 +0800

    add new file

---
+++ Chapter5/5.15.c
@@ -0,0 +1,25 @@
+//二分法 求根
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    double fx(double x);
+    double a = -10.0, b = 10, t;
+    while(fabs(b - a) > 1e-5)
+    {
+        t = (a+b)/2;
+        if( fx(a) * fx(t) < 0)
+            b = t;
+        else
+            a = t;
+    }
+    
+    printf("%lf\n",t);
+    return 0;
+
+}
+
+double fx(double x)
+{
+    return 2*x*x*x - 4*x*x + 3*x - 6;
+}
