69f1dc3927143dfd919e83817f6ee0a6b48e7f1a
V-T0 <keivong@outlook.com>
2021-10-23 19:22:57 +0800

    add new file

---
+++ Chapter5/5.10.1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    double n = 2.0, d = 1.0, term, sum = 0, t;
+    int i;
+    for(i = 1; i <= 20; i++)
+    {
+        term = n / d;
+        t = n;
+        n = n + d;
+        d = t;
+        sum = sum + term;
+    }
+
+    // printf("%.10lf\n",sum);
+    printf("%lf\n",sum);
+
+    return 0;
+    
+}
