84c12d05d19af1d86b17b20be9c39ddc3113acd3
V-T0 <keivong@outlook.com>
2021-10-23 15:42:04 +0800

    add new file

---
+++ Chapter5/5.3.c
@@ -0,0 +1,31 @@
+//使用辗转相除法计算最大公因数
+#include<stdio.h>
+int main()
+{
+    int m,n,r,t;
+    int a,b;
+    printf("输入两个正整数:");
+    scanf("%d,%d",&m,&n);
+    a = m;
+    b = n;
+    if( m < n){
+        t = m;
+        m = n;
+        n = t;
+    }
+    int gcd,lcm; //gcd greatest common divisor 最大公因数 lcm Least Common Multiple最小公倍数
+    do
+    {
+        r = m % n;
+        m = n;
+        gcd = n;
+        n = r;
+        
+    } while (r != 0);
+
+    lcm = (a * b) / gcd;
+
+    printf("最大公约数是:%d\t最小公倍数是:%d\n",gcd,lcm);
+    return 0;
+    
+}
