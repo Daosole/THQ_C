4d8bf3a584e0c8ade6e64435e0a55a2b610d0b9e
V-T0 <keivong@outlook.com>
2021-10-24 13:37:07 +0800

    add new file

--- Chapter5/5.5.c
+++ Chapter5/5.5.c
@@ -1,1 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int sum, n, a, i, b;
+    printf("数字 a, 位数 n\n");
+    scanf("%d,%d", &a, &n);
+    sum = b = a;
+    for (i = 1; i < n; i++)
+    {
+        // printf("%d\n", a);
+        a = (a * 10) + b;
+        sum = sum + a;
+    }
+
+    printf("S = %d\n", sum);
+    printf("\n");
+
+    return 0;
+}
