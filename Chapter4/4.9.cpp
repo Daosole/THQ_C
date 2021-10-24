b9c174e9de5d7e9ffa0b9e71494f26f98286603e
V-T0 <keivong@outlook.com>
2021-10-24 10:18:36 +0800

    final version

--- Chapter4/4.9.cpp
+++ Chapter4/4.9.cpp
@@ -1,22 +1,49 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main(void)
 {
-	int a,b,c,i;
-	scanf("%d",&a);
-	b = a;
-	i = 0;
-	printf("\n");
-	for(i = 0; b !=0 ; i++)
+	int a[5], b, c, i;
+	printf("请输入一个多于 5 位的正整数:");
+	scanf("%d", &b);
+	while (b > 99999 || b < 0)
 	{
-		c = b%10;
-		b = b/10;
-		printf("%d\n",c);
-		
+		printf("Error,input again\n");
+		scanf("%d", &b);
 	}
-	printf("\n");
+	int d1, d2, d3, d4, d5; // 万 千 百 十 个
+	d1 = b / 10000;
+	d2 = (b - d1 * 10000) / 1000;
+	d3 = (b - d1 * 10000 - d2 * 1000) / 100;
+	d4 = (b - d1 * 10000 - d2 * 1000 - d3 * 100) / 10;
+	d5 = b % 10;
+	printf("逆序输出:\n");
+	for (i = 0; b != 0; i++)
+	{
+		c = b % 10;
+		b = b / 10;
+		printf("%d\n", c);
+	}
 
-	printf("这是个%d位数\n",i);
-	printf("a=%d,%d,%d",a,b,c);
+	printf("这是个%d位数\n", i);
+
+	switch (i)
+	{
+	case 5:
+		printf("%d %d %d %d %d", d1, d2, d3, d4, d5);
+		break;
+	case 4:
+		printf("%d %d %d %d ", d2, d3, d4, d5);
+		break;
+	case 3:
+		printf("%d %d %d ", d3, d4, d5);
+		break;
+	case 2:
+		printf("%d %d", d4, d5);
+		break;
+	case 1:
+		printf("%d ", d5);
+		break;
+	}
+
+	printf("\n");
 	return 0;
-	
 }
