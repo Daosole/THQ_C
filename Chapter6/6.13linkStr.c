36456c844242d371571e07357cba696f674c961a
V-T0 <keivong@outlook.com>
2021-11-15 13:45:30 +0800

    update: add new file
    6.13

---
+++ Chapter6/6.13linkStr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char s1[100], s2[50];
+    printf("Input string one:\n");
+    gets(s1);
+    printf("Input string two:\n");
+    gets(s2);
+    int i = 0, j = 0;
+    i = strlen(s1);
+    while (s2[j] != '\0')
+    {
+        s1[i] = s2[j];
+        i++;
+        j++;
+    }
+    printf("%s", s1);
+
+    return 0;
+}
