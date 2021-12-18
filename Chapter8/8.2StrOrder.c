75af03aee2c6a3ff55bc8d8b524307d554fd20cc
V-T0 <keivong@outlook.com>
2021-12-19 07:04:07 +0800

    update: complete 8.2

--- Chapter8/8.2StrOrder.c
+++ Chapter8/8.2StrOrder.c
@@ -4,6 +4,7 @@
 int main(void)
 {
     void swap(char *, char *);
+    int strCmp(char *, char *);
     char s1[N], s2[N], s3[N];
     char *a, *b, *c;
     a = s1, b = s2, c = s3;
@@ -12,6 +13,38 @@ int main(void)
     gets(s1);
     gets(s2);
     gets(s3);
-    
+    if (strCmp(a, b) > 0) // s1 > s2 exchange
+        swap(a, b);
+    if (strCmp(a, c) > 0)
+        swap(a, c);
+    if (strCmp(b, c) > 0)
+        swap(b, c);
 
+    printf("After sorted:\n");
+    puts(a);
+    puts(b);
+    puts(c);
+
+    return 0;
+}
+
+int strCmp(char *p, char *q)
+{
+    int i = 0, result;
+    while (*(p + i) == *(q + i) && *(p + i) != '\0')
+    {
+        i++;
+    }
+    if (*(p + i) == '\0' && *(q + i) == '\0')
+        return result = 0;
+    else
+        return result = *(p + i) - *(q + i);
+}
+
+void swap(char *p, char *q)
+{
+    char *t[N];
+    strcpy(t, p);
+    strcpy(p, q); //字符数组的交换
+    strcpy(q, t);
 }
