5ad0893cc09e4383f23b74820e1977fdc557df2b
Daosole <keivong@outlook.com>
2022-03-12 20:04:36 +0800

    update: 8.16 - 19

---
+++ Example/C8/li8.16.c
@@ -0,0 +1,9 @@
+// 字符串引用
+#include <stdio.h>
+int main(void)
+{
+    char string[] = "I love China!";
+    printf("%s\n", string);
+    printf("%c\n", string[7]);
+    return 0;
+}
---
+++ Example/C8/li8.17.c
@@ -0,0 +1,8 @@
+// 通过字符指针变量输出一个字符串
+#include <stdio.h>
+int main(void)
+{
+    char *string = "I love China!";
+    printf("%s\n", string);
+    return 0;
+}
